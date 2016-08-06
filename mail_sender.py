c0687f4d48c32095c2ac71f7586d43d766fa0b23
codyowl <codyowl@gmail.com>
2016-08-07 00:20:08 +0530

    Intialized with test script

--- mail_sender.py
+++ mail_sender.py
@@ -1,1 +1,21 @@
-import os
+import smtplib
+
+USER_NAME =  
+PASSWORD = 
+
+SUBJECT = raw_input("Enter the subject :")
+
+CONTENT = raw_input("Enter the mail content :")
+
+FROM = 
+
+TO = 
+
+def mail_sender(username, password, message):
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.login(username, password)
+	server.sendmail(FROM, TO, message)
+	server.close()
+
+
+mail_sender(USER_NAME, PASSWORD, CONTENT)
